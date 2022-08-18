befaa1d644434e2e51cf85ec2522c967e9f9a91f
prabal-007 <gprabal123@gmail.com>
2022-08-18 17:06:18 +0530

    Project-5

--- Project-5_sticky_navbar/index.js
+++ Project-5_sticky_navbar/index.js
@@ -1,8 +1,6 @@
 const navbarEl = document.querySelector(".navbar");
 const bottomcontainerEl = document.querySelector(".bottom-container");
 
-// console.log(navbarEl.offsetHeight);
-
 window.addEventListener("scroll", ()=> {
     if (window.scrollY > bottomcontainerEl.offsetTop - navbarEl.offsetHeight - 50){
         navbarEl.classList.add("active");
